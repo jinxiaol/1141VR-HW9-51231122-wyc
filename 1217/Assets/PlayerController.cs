e90401b614a183b50d95fd2f6a6f1421e5348e67
jinxiao <wesleywu314@gmail.com>
2025-12-17 14:55:14 +0800

    1217

--- 1217/Assets/PlayerController.cs
+++ 1217/Assets/PlayerController.cs
@@ -6,6 +6,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
 
     void Start()
     {
@@ -18,6 +20,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        float speedx = Mathf.Abs(this.rigid2D.linearVelocity.x);
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
         }
     }
 }
